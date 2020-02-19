6a7c1fec6c70671f61d720a598de9b27e7b01255
Amaury <amaury.lecomte@epitech.eu>
2020-02-19 23:25:10 +0100

    Add functional test

---
+++ objdump/tests/functional/tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+
+test_passed=0
+test_crashed=0
+test_failed=0
+nb_test=0
+
+# $1 = path binary
+# $2 = description of the test
+exec_test()
+{
+    ((nb_test++))
+    echo -ne "${CYAN}${bold}Test n°$nb_test${NC} ${bold}($2)${NC} : "
+
+    # Get the expect value
+    expect=`objdump -fs $1`
+
+    # Get the actual value
+    result=`./my_objdump $1`
+
+    if [[ "$expect" == "$result" ]]
+    then
+        echo -ne "${GREEN}${bold}OK${NC}\n"
+        ((test_passed++))
+    else
+        echo -ne "${RED}${bold}KO${NC}\n"
+        ((test_failed++))
+    fi
+}
+
+exec_test "/bin/ls" "/bin/ls"
+exec_test "/bin/cat" "/bin/cat"
+exec_test "/bin/touch" "/bin/touch"
+
+exit 0

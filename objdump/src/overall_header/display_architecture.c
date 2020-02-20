be624e3d90755dde5462680455ee6634e18ea122
Amaury <amaury.lecomte@epitech.eu>
2020-02-20 14:49:59 +0100

    Change two const global variable to one const structure

--- objdump/src/overall_header/display_architecture.c
+++ objdump/src/overall_header/display_architecture.c
@@ -8,23 +8,38 @@
 #include "../../include/objdump.h"
 #include <stdio.h>
 
-const char defines[] = {
-    EM_M32, EM_SPARC, EM_386, EM_68K, EM_88K, EM_860, EM_MIPS, EM_PARISC, \
-    EM_SPARC32PLUS, EM_PPC, EM_PPC64, EM_S390, EM_ARM, EM_SH, EM_SPARCV9, \
-    EM_IA_64, EM_X86_64, EM_VAX
-};
+typedef struct architecture_s
+{
+    int define;
+    char *name;
+} architecture_t;
 
-const char *architectures[] = {
-    "m32", "sparc", "386", "68k", "88k", "860", "mips", "parisc", \
-    "sparc32plus", "ppc", "ppc64", "s390", "arm", "sh", "sparcv9", \
-    "ia_64", "x86-64", "vax", NULL
+const architecture_t architecture[] = {
+    {EM_M32, "m32"},
+    {EM_SPARC, "sparc"},
+    {EM_386, "386"},
+    {EM_68K, "68k"},
+    {EM_88K, "88k"},
+    {EM_860, "860"},
+    {EM_MIPS, "mips"},
+    {EM_PARISC, "parisc"},
+    {EM_SPARC32PLUS, "sparc32plus"},
+    {EM_PPC, "ppc"},
+    {EM_PPC64, "ppc64"},
+    {EM_S390, "s390"},
+    {EM_ARM, "arm"},
+    {EM_SH, "sh"},
+    {EM_SPARCV9, "sparcv9"},
+    {EM_IA_64, "ia-69"},
+    {EM_X86_64, "x86-64"},
+    {EM_VAX, "vax"}
 };
 
 void display_architecture(objdump_t *obj)
 {
-    for (size_t i = 0; architectures[i]; i++) {
-        if (obj->ehdr->e_machine == defines[i]) {
-            printf("architecture: i386:%s, ", architectures[i]);
+    for (size_t i = 0; i < 18; i++) {
+        if (obj->ehdr->e_machine == architecture[i].define) {
+            printf("architecture: i386:%s, ", architecture[i].name);
         }
     }
 }

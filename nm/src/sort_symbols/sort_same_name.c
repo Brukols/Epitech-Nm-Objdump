e01a73f7a44fad04c31996b49d30b263b8b59d29
Amaury <amaury.lecomte@epitech.eu>
2020-03-02 15:03:57 +0100

    Fix sort with maj

--- nm/src/sort_symbols/sort_same_name.c
+++ nm/src/sort_symbols/sort_same_name.c
@@ -6,13 +6,39 @@
 */
 
 #include "../../include/nm.h"
+#include <ctype.h>
+
+static int special_cmp(const char *s1, const char *s2)
+{
+    int result_cmp = strcasecmp(s1, s2);
+    size_t i_s1 = 0;
+    size_t i_s2 = 0;
+
+    if (result_cmp != 0)
+        return (result_cmp);
+    for (size_t i = 0; s1[i]; i++) {
+        for (; s1[i_s1] && !isalnum(s1[i_s1]); i_s1++);
+        for (; s2[i_s2] && !isalnum(s2[i_s2]); i_s2++);
+        if (s1[i_s1] > s2[i_s2])
+            return (-1);
+        if (s1[i_s1] < s2[i_s2])
+            return (1);
+        if (s1[i_s1])
+            i_s1++;
+        if (s2[i_s2])
+            i_s2++;
+    }
+    if (s1[i_s1] > s2[i_s2])
+        return (-1);
+    return ((s1[i_s1] < s2[i_s2] ? 1 : 0));
+}
 
 static symbol_t *compare_first(symbol_t *symbols, symbol_t **fs)
 {
     symbol_t *first = NULL;
     symbol_t *second = NULL;
 
-    if (strcmp(symbols->name, symbols->next->name) <= 0)
+    if (special_cmp(symbols->name, symbols->next->name) <= 0)
         return (symbols);
     first = symbols->next;
     second = symbols->next->next;
@@ -36,7 +62,7 @@ symbol_t *sort_same_name(symbol_t *symbols)
             last = compare_first(symbols, &first);
             symbols = symbols->next;
             continue;
-        } else if (strcmp(symbols->name, symbols->next->name) > 0)
+        } else if (special_cmp(symbols->name, symbols->next->name) > 0)
             symbols = swap_list(last);
         last = symbols;
         symbols = symbols->next;

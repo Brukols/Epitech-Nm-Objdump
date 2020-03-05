d2ce6cdd87020d27c2243816e9fbbc7f7b00905b
Amaury <amaury.lecomte@epitech.eu>
2020-03-05 12:35:14 +0100

    skip .a

--- nm/src/display_informations.c
+++ nm/src/display_informations.c
@@ -8,6 +8,17 @@
 #include "nm.h"
 #include <stdio.h>
 
+static bool is_a(const char *path)
+{
+    int lgt = strlen(path);
+
+    if (lgt <= 1)
+        return (false);
+    if (path[lgt - 1] == 'a' && path[lgt - 2] == '.')
+        return (true);
+    return (false);
+}
+
 int display_informations(int ac, char *path)
 {
     nm_t nm = init_elf_struct(path);
@@ -18,16 +29,16 @@ int display_informations(int ac, char *path)
         close_file(nm.fd);
         return (FAILURE);
     }
+    if (is_a(path))
+        return (close_file(nm.fd));
     if (init_elfs_struct(&nm) == FAILURE) {
         destroy_elf_struct(nm);
         return (FAILURE);
     }
     if (ac > 2)
         printf("\n%s:\n", path);
-    if (!nm.sym.sym32 && !nm.sym.sym64)
-        printf("nm: %s: no symbols\n", nm.path);
-    else
-        display_symbols(&nm);
+    (!nm.sym.sym32 && !nm.sym.sym64 ? printf("nm: %s: no symbols\n", nm.path) \
+: display_symbols(&nm));
     destroy_elf_struct(nm);
     return (SUCCESS);
 }
--- objdump/src/display_information.c
+++ objdump/src/display_information.c
@@ -7,6 +7,17 @@
 
 #include "objdump.h"
 
+static bool is_a(const char *path)
+{
+    int lgt = strlen(path);
+
+    if (lgt <= 1)
+        return (false);
+    if (path[lgt - 1] == 'a' && path[lgt - 2] == '.')
+        return (true);
+    return (false);
+}
+
 int display_information(char *path)
 {
     objdump_t obj = init_elf_struct(path);
@@ -16,6 +27,10 @@ int display_information(char *path)
     if (obj.buf == (void *)-1) {
         close_file(obj.fd);
         return (FAILURE);
+    }
+    if (is_a(path)) {
+        close_file(obj.fd);
+        return (SUCCESS);
     }
     if (init_elfs_struct(&obj) == FAILURE) {
         destroy_elf_struct(obj);
